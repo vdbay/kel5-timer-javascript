a6f5fab99443c86a91390fc366134f07258835a7
nabilputrah <61397812+nabilputrah@users.noreply.github.com>
2021-09-25 20:18:04 +0700

    push2

--- index.js
+++ index.js
@@ -1,81 +1,82 @@
-const timer = document.querySelector("#time");
-const start_btn = document.querySelector("#start_btn");
-const pause_btn = document.querySelector("#pause_btn");
-const reset_btn = document.querySelector("#reset_btn");
-const total = document.querySelector("#total");
+const timer = document.querySelector('#time');
+const start_btn = document.querySelector('#start_btn');
+const pause_btn = document.querySelector('#pause_btn');
+const reset_btn = document.querySelector('#reset_btn');
+const total = document.querySelector('#total');
 
 let time = 0,
-	interval;
+  interval;
 
 function showTime() {
-	time += 1;
-	timer.innerHTML = toHHMMSS(time);
+  time += 1;
+  timer.innerHTML = toHHMMSS(time);
 }
 
 function start() {
-	interval = setInterval(showTime, 1000);
-	hideBtn([start_btn]);
-	hideTotal(total);
-	showBtn([pause_btn, reset_btn]);
+  interval = setInterval(showTime, 1000);
+  hideBtn([start_btn]);
+  hideTotal(total);
+  showBtn([pause_btn, reset_btn]);
 }
 
 function pause() {
-	if (interval) {
-		clearInterval(interval);
-		interval = null;
-		pause_btn.innerHTML = "RESUME";
-	} else {
-		interval = setInterval(showTime, 1000);
-		pause_btn.innerHTML = "PAUSE";
-	}
+  if (interval) {
+    clearInterval(interval);
+    interval = null;
+    pause_btn.innerHTML = 'RESUME';
+  } else {
+    interval = setInterval(showTime, 1000);
+    pause_btn.innerHTML = 'PAUSE';
+  }
 }
 
 function reset() {
-	clearInterval(interval);
-	interval = null;
-	pause_btn.innerHTML = "PAUSE";
-	showTotal(total);
-	total.innerHTML = "Total Waktu Pengerjaan : " + toTotal(time);
-	time = 0;
-	timer.innerHTML = toHHMMSS(time);
-	hideBtn([pause_btn, reset_btn]);
-	showBtn([start_btn]);
+  clearInterval(interval);
+  interval = null;
+  pause_btn.innerHTML = 'PAUSE';
+  showTotal(total);
+  //total.innerHTML = 'Total waktu pengerjaan: '
+  total.innerHTML = 'Total: ' + toTotal(time) ;
+  time = 0;
+  timer.innerHTML = toHHMMSS(time);
+  hideBtn([pause_btn, reset_btn]);
+  showBtn([start_btn]);
 }
 
 function toHHMMSS(time) {
-	let hours = Math.floor(time / 3600);
-	let minutes = Math.floor((time - hours * 3600) / 60);
-	let seconds = time - hours * 3600 - minutes * 60;
+  let hours = Math.floor(time / 3600);
+  let minutes = Math.floor((time - hours * 3600) / 60);
+  let seconds = time - hours * 3600 - minutes * 60;
 
-	hours = `${hours}`.padStart(2, '0');
-	minutes = `${minutes}`.padStart(2, '0');
-	seconds = `${seconds}`.padStart(2, '0');
+  hours = `${hours}`.padStart(2, '0');
+  minutes = `${minutes}`.padStart(2, '0');
+  seconds = `${seconds}`.padStart(2, '0');
 
-	return hours + ':' + minutes + ':' + seconds;
+  return hours + ':' + minutes + ':' + seconds;
 }
 
 function toTotal(time) {
-	let hours = Math.floor(time / 3600);
-	let minutes = Math.floor((time - hours * 3600) / 60);
-	let seconds = time - hours * 3600 - minutes * 60;
-
-	hours = `${hours}`;
-	minutes = `${minutes}`;
-	seconds = `${seconds}`;
-
-	return hours + " Jam " + minutes + " Menit " + seconds + "Detik";
+    let hours = Math.floor(time / 3600);
+    let minutes = Math.floor((time - hours * 3600) / 60);
+    let seconds = time - hours * 3600 - minutes * 60;
+  
+    hours = `${hours}`;
+    minutes = `${minutes}`;
+    seconds = `${seconds}`;
+  
+    return hours + ' jam ' + minutes + ' menit ' + seconds + ' detik';
 }
 
 function showBtn(btnArr) {
-	btnArr.forEach((btn) => (btn.style.display = "inline-block"));
+  btnArr.forEach((btn) => (btn.style.display = 'inline-block'));
 }
 function hideBtn(btnArr) {
-	btnArr.forEach((btn) => (btn.style.display = "none"));
+  btnArr.forEach((btn) => (btn.style.display = 'none'));
 }
 function showTotal(Total) {
-	Total.style.display = "block";
+    Total.style.display = "block";
 }
 function hideTotal(Total) {
-	Total.style.display = "none";
+    Total.style.display = "none";
 }
 
